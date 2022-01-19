370cef741e805e993b543d48fdf45efe028fca06
Mariana Viana Rodriguez <workdeveloper.new@gmail.com>
2022-01-19 11:07:28 +0100

    added paint number errors

---
+++ src/components/App.js
@@ -0,0 +1,104 @@
+import '../styles/App.scss';
+import { useState } from 'react';
+
+function App() {
+  const [word, setWord] = useState('katakroker');
+  const [userLetters, setUserLetters] = useState([]);
+  const [lastLetter, setLastLetter] = useState([]);
+  
+
+  let wordLetters;
+
+  const handlerInput = (eve) => {
+    //escucho la letra de la usuaria
+    const valueInput = eve.currentTarget.value;
+    let er = new RegExp(/^[a-zA-ZáäéëíïóöúüÁÄÉËÍÏÓÖÚÜñÑ]?$/);
+    if (er.test(valueInput)) {
+      console.log(er.test(valueInput));
+      setLastLetter(valueInput)
+      userLetters.push(lastLetter);
+      setUserLetters(userLetters); 
+  }
+  //guardo en userLetters la ultima letra introducida por el usuario.
+  
+};
+
+  const renderSolutionLetters = () =>{
+   wordLetters = word.split('');
+  // const filter = wordLetters.filter(x => userLetters.includes(x) === true);
+    return wordLetters.map((wordLetter,index) => {
+      return <li key={index} className = "letter">
+        <small>{userLetters.includes(wordLetter)?wordLetter:""}</small>
+        </li>
+    })
+  }
+
+  const renderErrorLetters = () =>{
+    const filter = userLetters.filter(x => word.includes(x) === false);
+    return filter.map((wordLetter, index) => {
+      return <li key={index} className="letter">
+        <small>{wordLetter}</small>
+      </li>
+    })
+  };
+
+  const filterErrors = userLetters.filter(x => word.includes(x) === false);
+
+  return (
+    <div className="title">
+      <div className="page">
+        <header>
+          <h1 className="header__title">Juego del ahorcado</h1>
+        </header>
+        <main className="main">
+          <section>
+            <div className="solution">
+              <h2 className="title">Solución:</h2>
+              <ul className="letters">
+                {renderSolutionLetters()}
+              </ul>
+            </div>
+            <div className="error">
+              <h2 className="title">Letras falladas:</h2>
+              <ul className="letters">
+                {renderErrorLetters()}
+              </ul>
+            </div>
+            <form className="form">
+              <label className="title" htmlFor="last-letter">
+                Escribe una letra:
+              </label>
+              <input
+                autoComplete="off"
+                className="form__input"
+                maxLength="1"
+                type="text"
+                name="last-letter"
+                id="last-letter"
+                onChange={handlerInput}
+                value={lastLetter}
+              />
+            </form>
+          </section>
+          <section className={`dummy error-${filterErrors.length}`}>
+            <span className="error-13 eye"></span>
+            <span className="error-12 eye"></span>
+            <span className="error-11 line"></span>
+            <span className="error-10 line"></span>
+            <span className="error-9 line"></span>
+            <span className="error-8 line"></span>
+            <span className="error-7 line"></span>
+            <span className="error-6 head"></span>
+            <span className="error-5 line"></span>
+            <span className="error-4 line"></span>
+            <span className="error-3 line"></span>
+            <span className="error-2 line"></span>
+            <span className="error-1 line"></span>
+          </section>
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default App;
