232f81da7f3a7a0a300c0a85b8900685c87f3b70
Your Name <you@example.com>
2022-01-29 12:05:14 +0100

    intructions

--- src/components/Intructions.js
+++ src/components/Intructions.js
@@ -2,10 +2,10 @@ const Intructions = () => {
   return (
     <section className="instructions">
       <p>
-        Introduce una letra y si fallas se va rellenando el muñequito, cuando se complete habŕas perdido.
+        El ordenador generará una palabra aleatoria que deberás adivinar, por cada letra fallada, el muñeco se irá rellenando de color blanco, si consigues acertar la letra antes de que se coloreé el muñeco... significa que habrás ganado ¡Enhorabuena!        
       </p>
       <p>
-        Si quieres jugar con alguien más en la sesión de opciones puedes poner un palabra a adivinar y jugar junt@s.
+        Para jugar con un amigo o familiar, haz click en más opciones y elige tu mismo la palabra que tendrá que adivinar. Tú eliges el nivel de dificultad. ¡A jugar!
       </p>
     </section>
   );
